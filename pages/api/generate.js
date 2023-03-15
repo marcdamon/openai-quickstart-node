dea32074c03b12d6bfb21993c84e721bcc5d47ad
marcdamon <mdamon@me.com>
2023-03-15 12:54:22 -0500

    try again

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -30,7 +30,7 @@ export default async function (req, res) {
       model: "text-davinci-003",
       prompt: generatePrompt(animal),
       temperature: 0.6,
-      n: 4,
+      n: 7,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
